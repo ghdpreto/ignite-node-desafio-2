7ece0e788f6bd63861eb4c0e73f88b5eeee72776
Gustavo Henrique <ghdpreto@gmail.com>
2023-05-30 15:22:56 -0300

    chore: ajuste deploy prd

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,14 @@
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
 
 export const configDb: Knex.Config = {
-  client: 'sqlite3',
-  connection: {
-    filename: './db/dev.db',
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
